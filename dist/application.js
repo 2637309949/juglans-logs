c56d7475ebc53ddff5a38bb15cfbb1f70f3f945d
double <2637309949@qq.com>
2019-04-04 15:59:26 +0800

    logger

--- dist/application.js
+++ dist/application.js
@@ -19,6 +19,8 @@ const moment = require('moment');
 
 const utils = require('./utils');
 
+const logger = require('../../juglans-addition').logger;
+
 function formatPrint(_ref) {
   let {
     formatTime,
@@ -39,10 +41,12 @@ module.exports = (_ref2) => {
       var _ref4 = _asyncToGenerator(function* (_ref3) {
         let {
           router,
-          config: {
-            prefix
-          }
+          config
         } = _ref3;
+        const {
+          prefix
+        } = config;
+        const httpLogger = logger.createHttpLogger(config).http;
         assert.ok(is.function(record), 'Record parameter should be func type');
         router.use(
         /*#__PURE__*/
@@ -66,30 +70,44 @@ module.exports = (_ref2) => {
                 reqForm.accessData = accessData;
 
                 if (reqForm.requestUrl.startsWith(prefix)) {
-                  formatPrint({
+                  logInfo = formatPrint({
                     formatTime,
                     type: reqForm.accessData.username,
                     method: reqForm.method,
                     href: reqForm.href
                   });
-                  logInfo = `${formatTime} [${reqForm.accessData.username}] ${reqForm.method} ${reqForm.href}`;
                   record(reqForm);
                 }
               } else if (ctx.state.fakeToken) {
-                logInfo = `${formatTime} [FT]: ${reqForm.method} ${reqForm.href}`;
+                logInfo = formatPrint({
+                  formatTime,
+                  type: 'FT',
+                  method: reqForm.method,
+                  href: reqForm.href
+                });
               } else if (ctx.state.fakeUrl) {
-                logInfo = `${formatTime} [FU]: ${reqForm.method} ${reqForm.href}`;
+                logInfo = formatPrint({
+                  formatTime,
+                  type: 'FU',
+                  method: reqForm.method,
+                  href: reqForm.href
+                });
               } else {
-                logInfo = `${formatTime} [UD]: ${reqForm.method} ${reqForm.href}`;
+                logInfo = formatPrint({
+                  formatTime,
+                  type: 'UD',
+                  method: reqForm.method,
+                  href: reqForm.href
+                });
               }
 
-              console.log(`=> ${logInfo}`);
+              httpLogger(`=> ${logInfo}`);
               yield next();
               const {
                 delta,
                 status
               } = utils.measure(start, Date.now(), ctx);
-              console.log(`<= ${logInfo} ${status} ${delta}`);
+              httpLogger(`<= ${logInfo} ${status} ${delta}`);
             } catch (err) {
               throw err;
             }
