172b3384fa7d58b6f68ea30ab5f5fd63892450f2
double <2637309949@qq.com>
2019-06-17 15:49:03 +0800

    style: mit

--- dist/utils.js
+++ dist/utils.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Copyright (c) 2018-2020 Double.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
 const repo = module.exports;
 
 repo.measure = function (start, end, ctx) {
